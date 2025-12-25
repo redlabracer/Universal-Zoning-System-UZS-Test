2dde1e360262b745c0ce26de2685c90e4e541972
redlabracer <redlabracer@gmail.com>
2025-12-25 14:30:44 +0100

    Fixed the settings menu looking weired

--- Universal Zoning System (UZS)/Localization/LocalizationManager.cs
+++ Universal Zoning System (UZS)/Localization/LocalizationManager.cs
@@ -58,48 +58,49 @@ namespace UniversalZoningSystem.Localization
             yield return new KeyValuePair<string, string>("Assets.NAME[UZS_HighOffice]", "Universal High Office");
             yield return new KeyValuePair<string, string>("Assets.DESCRIPTION[UZS_HighOffice]", "Corporate towers and business centers globally.");
 
-            // Settings
-            yield return new KeyValuePair<string, string>("Options.SECTION[UniversalZoningSystem.UniversalZoningSystem.Mod]", "Universal Zoning System");
+            // Settings - Keys must match the format CS2 generates: "ModDisplayName.Namespace.TypeName.Property"
+            // Based on assembly name "Universal Zoning System (UZS)" and the ModSettings class location
+            yield return new KeyValuePair<string, string>("Options.SECTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod]", "Universal Zoning System");
 
-            yield return new KeyValuePair<string, string>("Options.GROUP[UniversalZoningSystem.UniversalZoningSystem.Mod.RegionSettings]", "Region Settings");
-            yield return new KeyValuePair<string, string>("Options.GROUP[UniversalZoningSystem.UniversalZoningSystem.Mod.WeightingSettings]", "Weighting Settings");
-            yield return new KeyValuePair<string, string>("Options.GROUP[UniversalZoningSystem.UniversalZoningSystem.Mod.AdvancedSettings]", "Advanced Settings");
+            yield return new KeyValuePair<string, string>("Options.GROUP[Universal Zoning System (UZS).UniversalZoningSystem.Mod.RegionSettings]", "Region Settings");
+            yield return new KeyValuePair<string, string>("Options.GROUP[Universal Zoning System (UZS).UniversalZoningSystem.Mod.WeightingSettings]", "Weighting Settings");
+            yield return new KeyValuePair<string, string>("Options.GROUP[Universal Zoning System (UZS).UniversalZoningSystem.Mod.AdvancedSettings]", "Advanced Settings");
 
-            yield return new KeyValuePair<string, string>("Options.OPTION[UniversalZoningSystem.Settings.ModSettings.EnableNorthAmerica]", "North America");
-            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[UniversalZoningSystem.Settings.ModSettings.EnableNorthAmerica]", "Include North American buildings");
+            yield return new KeyValuePair<string, string>("Options.OPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.EnableNorthAmerica]", "North America");
+            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.EnableNorthAmerica]", "Include North American buildings");
 
-            yield return new KeyValuePair<string, string>("Options.OPTION[UniversalZoningSystem.Settings.ModSettings.EnableEuropean]", "European");
-            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[UniversalZoningSystem.Settings.ModSettings.EnableEuropean]", "Include European buildings");
+            yield return new KeyValuePair<string, string>("Options.OPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.EnableEuropean]", "European");
+            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.EnableEuropean]", "Include European buildings");
 
-            yield return new KeyValuePair<string, string>("Options.OPTION[UniversalZoningSystem.Settings.ModSettings.EnableUnitedKingdom]", "United Kingdom");
-            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[UniversalZoningSystem.Settings.ModSettings.EnableUnitedKingdom]", "Include UK buildings");
+            yield return new KeyValuePair<string, string>("Options.OPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.EnableUnitedKingdom]", "United Kingdom");
+            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.EnableUnitedKingdom]", "Include UK buildings");
 
-            yield return new KeyValuePair<string, string>("Options.OPTION[UniversalZoningSystem.Settings.ModSettings.EnableGermany]", "Germany");
-            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[UniversalZoningSystem.Settings.ModSettings.EnableGermany]", "Include German buildings");
+            yield return new KeyValuePair<string, string>("Options.OPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.EnableGermany]", "Germany");
+            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.EnableGermany]", "Include German buildings");
 
-            yield return new KeyValuePair<string, string>("Options.OPTION[UniversalZoningSystem.Settings.ModSettings.EnableFrance]", "France");
-            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[UniversalZoningSystem.Settings.ModSettings.EnableFrance]", "Include French buildings");
+            yield return new KeyValuePair<string, string>("Options.OPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.EnableFrance]", "France");
+            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.EnableFrance]", "Include French buildings");
 
-            yield return new KeyValuePair<string, string>("Options.OPTION[UniversalZoningSystem.Settings.ModSettings.EnableNetherlands]", "Netherlands");
-            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[UniversalZoningSystem.Settings.ModSettings.EnableNetherlands]", "Include Dutch buildings");
+            yield return new KeyValuePair<string, string>("Options.OPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.EnableNetherlands]", "Netherlands");
+            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.EnableNetherlands]", "Include Dutch buildings");
 
-            yield return new KeyValuePair<string, string>("Options.OPTION[UniversalZoningSystem.Settings.ModSettings.EnableEasternEurope]", "Eastern Europe");
-            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[UniversalZoningSystem.Settings.ModSettings.EnableEasternEurope]", "Include Eastern European buildings");
+            yield return new KeyValuePair<string, string>("Options.OPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.EnableEasternEurope]", "Eastern Europe");
+            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.EnableEasternEurope]", "Include Eastern European buildings");
 
-            yield return new KeyValuePair<string, string>("Options.OPTION[UniversalZoningSystem.Settings.ModSettings.EnableJapan]", "Japan");
-            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[UniversalZoningSystem.Settings.ModSettings.EnableJapan]", "Include Japanese buildings");
+            yield return new KeyValuePair<string, string>("Options.OPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.EnableJapan]", "Japan");
+            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.EnableJapan]", "Include Japanese buildings");
 
-            yield return new KeyValuePair<string, string>("Options.OPTION[UniversalZoningSystem.Settings.ModSettings.EnableChina]", "China");
-            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[UniversalZoningSystem.Settings.ModSettings.EnableChina]", "Include Chinese buildings");
+            yield return new KeyValuePair<string, string>("Options.OPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.EnableChina]", "China");
+            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.EnableChina]", "Include Chinese buildings");
 
-            yield return new KeyValuePair<string, string>("Options.OPTION[UniversalZoningSystem.Settings.ModSettings.UseEqualRegionWeighting]", "Equal Region Weighting");
-            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[UniversalZoningSystem.Settings.ModSettings.UseEqualRegionWeighting]", "Give each region equal chance");
+            yield return new KeyValuePair<string, string>("Options.OPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.UseEqualRegionWeighting]", "Equal Region Weighting");
+            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.UseEqualRegionWeighting]", "Give each region equal chance");
 
-            yield return new KeyValuePair<string, string>("Options.OPTION[UniversalZoningSystem.Settings.ModSettings.EnableVerboseLogging]", "Verbose Logging");
-            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[UniversalZoningSystem.Settings.ModSettings.EnableVerboseLogging]", "Enable detailed logging");
+            yield return new KeyValuePair<string, string>("Options.OPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.EnableVerboseLogging]", "Verbose Logging");
+            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.EnableVerboseLogging]", "Enable detailed logging");
 
-            yield return new KeyValuePair<string, string>("Options.OPTION[UniversalZoningSystem.Settings.ModSettings.ResetToDefaults]", "Reset to Defaults");
-            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[UniversalZoningSystem.Settings.ModSettings.ResetToDefaults]", "Reset all settings");
+            yield return new KeyValuePair<string, string>("Options.OPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.ResetToDefaults]", "Reset to Defaults");
+            yield return new KeyValuePair<string, string>("Options.OPTION_DESCRIPTION[Universal Zoning System (UZS).UniversalZoningSystem.Mod.ModSettings.ResetToDefaults]", "Reset all settings");
         }
 
         public void Unload()
